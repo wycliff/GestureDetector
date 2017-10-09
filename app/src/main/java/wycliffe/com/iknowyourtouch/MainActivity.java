74d073f8279a752548210826a41550bdd829ad7c
mark <makunomark@gmail.com>
2017-10-09 15:23:57 +0300

    implemented touches

--- app/src/main/java/wycliffe/com/iknowyourtouch/MainActivity.java
+++ app/src/main/java/wycliffe/com/iknowyourtouch/MainActivity.java
@@ -2,6 +2,7 @@ package wycliffe.com.iknowyourtouch;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -10,14 +11,16 @@ import android.widget.TextView;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
 
     private TextToSpeech tts;
     private TextView txtText;
-    private GestureDetector mGestureDetector;
+    private GestureDetectorCompat mDetector;
+    private static String DEBUG_TAG = "Gesture detector";
 
     // Strings
-    String responseSingleTouch = "You have touched me";
+    String responseWhenTouched = "You have %s me";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,89 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         tts = new TextToSpeech(this, this);
         txtText = (TextView) findViewById(R.id.txtText);
         // Bind the gestureDetector to GestureListener
-        mGestureDetector = new GestureDetector(this, new GestureListener());
+        mDetector = new GestureDetectorCompat(this, this);
+        mDetector.setOnDoubleTapListener(this);
+    }
+
+
+    private void outPut(String output) {
+        String formattedOutput = String.format(responseWhenTouched, output);
+        speakOut(formattedOutput);
+        writeOut(formattedOutput);
+    }
+
+    private void speakOut(String speakOut) {
+        tts.setSpeechRate(2);
+        tts.speak(speakOut, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void writeOut(String textOut) {
+        txtText.setText(textOut);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+        outPut("long pressed");
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        outPut("scrolled");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        outPut("double tapped");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        outPut("single tapped");
+        return true;
     }
 
 
@@ -41,7 +126,6 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onDestroy();
     }
 
-
     //listens if TextToSpeech is initialized successfully
     @Override
     public void onInit(int status) {
@@ -51,37 +135,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                     || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.i("TTS", "This Language is not supported");
             } else {
-                speakOut();
+                speakOut("Text to speech ready");
+                writeOut("Text to speech ready");
             }
         } else {
             Log.e("TTS", "Initialization Failed!");
         }
 
-    }
-
-
-    private void speakOut() {
-        tts.setSpeechRate(2);
-        tts.speak(responseSingleTouch, TextToSpeech.QUEUE_FLUSH, null);
-    }
-
-    private void writeOut() {
-        txtText.setText(responseSingleTouch);
-    }
-
-    // onTouch() method gets called each time you perform any touch event with screen
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        //method onTouchEvent of GestureDetector class Analyzes the given motion event
-        //and if applicable triggers the appropriate callbacks on the GestureDetector.OnGestureListener supplied.
-        //Returns true if the GestureDetector.OnGestureListener consumed the event, else false.
-
-        boolean eventConsumed = mGestureDetector.onTouchEvent(event);
-        if (eventConsumed) {
-            speakOut();
-            writeOut();
-            return true;
-        }
-        return false;
     }
 }
